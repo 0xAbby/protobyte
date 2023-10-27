dca5425df6dc134a120a72cfc7aeb6d6f44ff1f3
0xAbby <128301871+0xAbby@users.noreply.github.com>
2023-10-27 16:19:25 +0300

    added basic headers and definitions.

---
+++ src/file_io.h
@@ -0,0 +1,28 @@
+// file_io.h:
+//    definitions for class / functions used to interact with the file,
+//    reading and writing operations.
+//
+// Author:
+//  Abdullah Ada 
+// 
+
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include "headers.h"
+
+class FileIO {
+private:
+  FILE *in;
+
+public:
+  FileIO(int argc, char *argv[]);
+  ~FileIO();
+
+
+
+  uint8_t read8_le(FILE *in);
+};
+
+
+#endif
---
+++ src/headers.h
@@ -0,0 +1,20 @@
+//
+// headers.h
+//
+// Author:
+//  Abdullah Ada
+//
+#ifndef HEADERS_H
+#define HEADERS_H
+ 
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <cstdint>
+#include <cstring>
+
+#include "pe.h"
+#include "file_io.h"
+
+
+#endif
