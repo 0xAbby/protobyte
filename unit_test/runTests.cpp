b6991e4b59212dfeab1ce54b79de7582ea274f30
0xAbby <128301871+0xAbby@users.noreply.github.com>
2023-11-19 20:06:14 +0200

    adding unit tests to improve PE parsing.

--- unit_test/runTests.cpp
+++ unit_test/runTests.cpp
@@ -12,17 +12,17 @@ public:
     }
 };
 
-TEST_F(PETest, DosHeader) {
-    ASSERT_EQ(pe.getDosMagic(), 0x5a4d);
-}
+TEST_F(PETest, DosHeader) { ASSERT_EQ(pe.getDosMagic(), 0x5a4d); }
 
-TEST_F(PETest, e_lfanew) {
-    ASSERT_EQ(pe.getElfanew(), 0x00000110);
-}
+TEST_F(PETest, e_lfanew) { ASSERT_EQ(pe.getElfanew(), 0x00000110); }
 
-TEST_F(PETest, PESignature) {
-    ASSERT_EQ(pe.getPESignature(), 0x4550);
-}
+TEST_F(PETest, PESignature) { ASSERT_EQ(pe.getPESignature(), 0x4550); }
+
+TEST_F(PETest, MachineType) { ASSERT_EQ(pe.getMachineType(), 0x8664); }
+
+TEST_F(PETest, NumberOfSections) { ASSERT_EQ(pe.getNumberOfSections(), 8); }
+
+TEST_F(PETest, DllCharacteristics) { ASSERT_EQ(pe.getDllCharacterics(), 0x4160); }
 
 int main(int argc, char*argv[]) {
     testing::InitGoogleTest(&argc, argv);
